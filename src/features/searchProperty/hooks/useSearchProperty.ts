be8d514478f650b7ba4528999b2b9c40a4fdbb5e
dikamuharman <dika1254@gmail.com>
2024-11-27 15:19:11 +0700

    Refactor useSearchProperty and SearchPage components

--- src/features/searchProperty/hooks/useSearchProperty.ts
+++ src/features/searchProperty/hooks/useSearchProperty.ts
@@ -1,15 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
-import { useSearchParams } from "react-router-dom";
-import searchService from "../service/searchService";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
+import searchService from '../service/searchService';
 
 const useSearchProperty = () => {
   const [params] = useSearchParams();
-  const lat = params.get("lat");
-  const lng = params.get("lng");
-  const prompt = params.get("prompt");
+  const lat = params.get('lat');
+  const lng = params.get('lng');
+  const prompt = params.get('prompt');
+
+  const queryClient = useQueryClient();
+
+  const queryKey = ['search', lat, lng, prompt];
 
   const querySearch = useQuery({
-    queryKey: ["search"],
+    queryKey: ['search'],
     queryFn: () => {
       return prompt == null
         ? searchService.searchPropertyByLocation([Number(lat), Number(lng)])
@@ -17,7 +21,16 @@ const useSearchProperty = () => {
     },
   });
 
-  return querySearch;
+  const mutation = useMutation({
+    mutationFn: (geom: number[][]) => {
+      return searchService.searchPropertyByArea(geom);
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(queryKey, data);
+    },
+  });
+
+  return { propertySearch: querySearch, propertyAreaMutation: mutation };
 };
 
 export default useSearchProperty;
